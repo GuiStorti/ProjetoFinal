42fb183116821a0b2ab881bbad770e18e449364a
Guilherme Luiz Storti do carmo <gui.storti@live.com>
2017-12-17 22:36:21 -0200

    ZzzZz

--- ProjetoFinal/src/main/java/sistema/beans/PedidoManagedBean.java
+++ ProjetoFinal/src/main/java/sistema/beans/PedidoManagedBean.java
@@ -11,7 +11,9 @@ import javax.faces.model.DataModel;
 import org.primefaces.event.RowEditEvent;
 
 import sistema.beans.datamodel.PedidoDataModel;
-//import sistema.modelos.Produto;
+import sistema.modelos.Produto;
+import sistema.modelos.Cliente;
+import sistema.modelos.Vendedor;
 import sistema.modelos.ProdutoPedido;
 import sistema.modelos.Pedido;
 import sistema.service.PedidoService;
@@ -21,23 +23,76 @@ import sistema.service.PedidoService;
 public class PedidoManagedBean {
 	
 	private Pedido pedido = new Pedido();
-	private Pedido pedidoSelecionada;
+	private Pedido pedidoSelecionado;
 	private PedidoService servico = new PedidoService();
 	private List<Pedido> pedidos;
+	private Cliente cliente;
+	private List<Cliente> clientes;
+	private Vendedor vendedor;
+	private List<Vendedor> vendedores;
+	private Produto produto;
+	private List<Produto> produtos;
 	
+	
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+	public List<Vendedor> getVendedores() {
+		return vendedores;
+	}
+	public void setVendedores(List<Vendedor> vendedores) {
+		this.vendedores = vendedores;
+	}
 	public Pedido getPedido() {
 		return pedido;
 	}
 	public void setPedido(Pedido pedido) {
 		this.pedido = pedido;
 	}
-	public Pedido getPedidoSelecionada() {
-		return pedidoSelecionada;
+	public Pedido getPedidoSelecionado() {
+		return pedidoSelecionado;
 	}
-	public void setPedidoSelecionada(Pedido pedidoSelecionada) {
-		this.pedidoSelecionada = pedidoSelecionada;
+	public void setPedidoSelecionado(Pedido pedidoSelecionado) {
+		this.pedidoSelecionado = pedidoSelecionado;
 	}
 	
+	public PedidoService getServico() {
+		return servico;
+	}
+	public void setServico(PedidoService servico) {
+		this.servico = servico;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 	public void salvar()
 	{
 		servico.salvar(pedido);
@@ -70,9 +125,9 @@ public class PedidoManagedBean {
 	}
 	
 	public List <ProdutoPedido> getProdutosPedido() {
-		if(pedidoSelecionada != null)
+		if(pedidoSelecionado != null)
 		{
-			return servico.pesquisarProdutosPedido(pedidoSelecionada);
+			return servico.pesquisarProdutosPedido(pedidoSelecionado);
 		}
 		else
 			return null;
